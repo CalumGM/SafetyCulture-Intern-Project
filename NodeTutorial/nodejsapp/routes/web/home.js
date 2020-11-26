de40f02c08b81daf51b9c0b81497e18587a42100
calumGM <calum.maitland00@gmail.com>
2020-11-26 15:48:43 +1000

    add login and signup pages

--- NodeTutorial/nodejsapp/routes/web/home.js
+++ NodeTutorial/nodejsapp/routes/web/home.js
@@ -4,17 +4,23 @@ var router = express.Router();
 
 
 router.get("/", function(req, res) {
-    // console.log("hello I'm on the start page");
- res.render("home/");
+ res.render("home/"); // default view
  });
  
  router.get("/home", function(req,res){
-     res.render("home/home");
+     res.render("home/home"); // home view
  });
 
  router.get("/about", function(req, res){
-    res.render("home/about");
+    res.render("home/about"); // about view
  });
 
+ router.get("/login", function(req, res){
+    res.render("home/login"); // Log In view
+ });
+
+ router.get("/signup", function(req, res){
+    res.render("home/signup"); // Log In view
+ });
 
  module.exports = router;
