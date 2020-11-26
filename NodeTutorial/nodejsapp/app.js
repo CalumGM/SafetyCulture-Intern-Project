2d7fc026f1b3e5375de96573ed83e83c14aff39a
calumGM <calum.maitland00@gmail.com>
2020-11-26 11:07:01 +1000

    Node app and ejs views

---
+++ NodeTutorial/nodejsapp/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+var routes = require("./routes")
+
+var app = express();
+
+console.log("Working");
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views")); // views are located in the "views" folder
+app.set("view engine", "ejs"); // which views engine is being used
+app.use(routes)
+
+app.listen(app.get("port"), function(){ // connection callback
+    console.log("Server started on port " + app.get("port"));
+});
