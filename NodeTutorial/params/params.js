83305010bd31b149adef22b15871ef878b5a1034
calumGM <calum.maitland00@gmail.com>
2020-11-27 13:50:17 +1000

    login, signup and graphs

---
+++ NodeTutorial/params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://calum_maitland:BenzacAC00@cluster0.qg16e.mongodb.net/RealEstateData?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
