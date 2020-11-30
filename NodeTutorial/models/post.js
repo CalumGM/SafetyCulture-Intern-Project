1016facd0bbbd2cde9ff3454f875d68830637c26
calumGM <calum.maitland00@gmail.com>
2020-12-01 09:02:45 +1000

    add and view posts

--- NodeTutorial/models/post.js
+++ NodeTutorial/models/post.js
@@ -4,11 +4,11 @@ var postSchema = mongoose.Schema({
     title: {type: String, required:true},
     content: {type: String, required:false},
     createdAt: {type: Date, default:Date.now},
-    image: {type: mongoose.Schema.Types.ObjectId, required:false, unique:flase}, // reference to the ID of another object
-    userID: {type:mongoose.Schema.Types.ObjectId, required:false, unique, flase}, // userID is used to refer to the post's user
+    image: {type:mongoose.Schema.Types.ObjectId, required:false, unique:false}, // reference to the ID of another object
+    userID: {type:mongoose.Schema.Types.ObjectId, required:false, unique:false}, // userID is used to refer to the post's user
     public: {type:Boolean, default:false, required:false, unique:false}
 });
 
-var Post = mongoose.model("Post", postSchema);
+var Post = mongoose.model("Post", postSchema); // collection is named after this with an s chucked on the end
 
 module.exports = Post;
