291f23cdbb6591e5917f0611e1810cb6260571d8
calumGM <calum.maitland00@gmail.com>
2020-12-01 13:40:58 +1000

    change database auth

--- NodeTutorial/params/params.js
+++ NodeTutorial/params/params.js
@@ -1,4 +1,4 @@
-const DATABASECONNECTION = "mongodb+srv://calum_maitland:BenzacAC00@cluster0.qg16e.mongodb.net/RealEstateData?retryWrites=true&w=majority"
+const DATABASECONNECTION = "mongodb+srv://calum_maitland:InternDatabase@cluster0.qg16e.mongodb.net/RealEstateData?retryWrites=true&w=majority"
 
 module.exports = {
     DATABASECONNECTION
