2425ff22d2a2e2e82c28ae0351dccfe64f18e682
calumGM <calum.maitland00@gmail.com>
2020-12-03 13:55:22 +1000

    before I ruin everything

---
+++ NodeTutorial/models/audits.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var auditSchema = mongoose.Schema({
+    audit_id: {type: String, required:true},
+    agent_name: {type: String, required:true},
+    data: {type: Date, default:Date.now},
+    location: {type:mongoose.Schema.Types.Object, required:false, unique:false},
+    score: {type:mongoose.Schema.Types.Object, required:false, unique:false},
+});
+
+var Audit = mongoose.model("audits", auditSchema); // collection is named after this with an s chucked on the end
+
+module.exports = Audit;
