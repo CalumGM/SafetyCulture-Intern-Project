484e35f3ea6b284898ff3d8eb2841bfbab1faf2b
Christgo <christopher.ordorica@my.jcu.edu.au>
2020-12-07 13:54:49 +1000

    Remove some print functions, remove some comments, add code to write_to_db()

--- Intern Project/EC2-PythonScripts/query_tables.py
+++ Intern Project/EC2-PythonScripts/query_tables.py
@@ -11,8 +11,6 @@ DATABASE_URL = "mongodb+srv://calum_maitland:InternDatabase@cluster0.qg16e.mongo
 def main():
     """main bruh"""
     # DOTO LIST:
-    # TODO pull all agents from agents db and if agent not in unique_agents: add a 0 to time-series with update
-    # TODO code will not account for a new agent joining on a date (that agent will have smaller array: needs processing)
     db_client, db_retrieve_col, db_audits_col, db_agents_col = db_connect()  # setup collections for other functions to use
     audit_dict_list, unique_agents = reformat_audits(db_retrieve_col=db_retrieve_col)
 
@@ -21,13 +19,11 @@ def main():
     agent_pull = emulate_agent_db()  # emulates the db for code construction purposes
 
     agent_dict_list = daily_agent_transform(unique_agents=unique_agents, db_emulation=agent_pull,
-                                            audit_dict_list=audit_dict_list, db_agents_col=db_agents_col)  # WIP function
-    # pull all agents
+                                            audit_dict_list=audit_dict_list, db_agents_col=db_agents_col)
+    # pull all agents for processing in write_to_db()
     all_agents = db_retrieve_col.find({}, {"_id": 0, "agent_name": 1})
-    # need code that will determine if agent is in db, update if he is else take dictionary created in code and insert
 
-
-    write_to_db(db_audits_col=db_audits_col, db_agents_col=db_agents_col, audit_dict_list=audit_dict_list, agent_dict_list=agent_dict_list, all_agents=all_agents, unique_agents=unique_agents)
+    # write_to_db(db_audits_col=db_audits_col, db_agents_col=db_agents_col, audit_dict_list=audit_dict_list, agent_dict_list=agent_dict_list, all_agents=all_agents, unique_agents=unique_agents)
     db_client.close()
 
 
@@ -80,7 +76,6 @@ def reformat_audits(db_retrieve_col):
                                                   "%Y-%m-%dT%H:%I:%S.%fZ")
         datetime_var = datetime.datetime.strftime(datetime_var, "%Y-%m-%d")
         datetime_var = datetime.datetime.strptime(datetime_var, "%Y-%m-%d")
-        print(datetime_var)
 
         # date = datetime_var.date()
         agent_name = audit["header_items"][2]["responses"]["text"]
@@ -97,7 +92,7 @@ def reformat_audits(db_retrieve_col):
                           "location": {"text": address, "lat": lat, "long": long}}
         audit_dict_list.append(new_audit_dict)
 
-    # get only unique agents
+    # convert all names to only unique agents
     set1 = set(agent_list)
     unique_agent_list = (list(set1))
 
@@ -110,8 +105,6 @@ def daily_agent_transform(unique_agents, db_emulation, audit_dict_list, db_agent
     # TODO: will need a way to store info for writing to db
     agent_dict_list = []
     for agent in unique_agents:
-        # TODO db query for info using agent name
-
         # get old scores
         historical_score = float(db_emulation[0]["avg_score"])
         historical_inspection_count = int(db_emulation[0]["total_inspection_count"])
@@ -126,11 +119,10 @@ def daily_agent_transform(unique_agents, db_emulation, audit_dict_list, db_agent
         # get new data
         count = 0
         audit_daily_score = []
-        for audit in audit_dict_list:
+        for audit in audit_dict_list:  # bit inefficient
             if audit["agent_name"] == agent:
-                # get stuff
                 daily_score = float(audit["scores"]["score_percentage"])
-                count += 1  # todo check to see if this works as intended
+                count += 1
                 audit_daily_score.append(daily_score)
 
         daily_score = sum(audit_daily_score)
@@ -157,7 +149,7 @@ def write_to_db(db_audits_col, db_agents_col, audit_dict_list, agent_dict_list, 
     #     dbEntry = audit
     #     db_audits_col.insert_one(dbEntry)
 
-    # db_audits_col.insert_many(audit_dict_list)  # use this one
+    db_audits_col.insert_many(audit_dict_list)  # use this one
 
     # three conditions:
     # agent exists and has an audit
@@ -165,18 +157,19 @@ def write_to_db(db_audits_col, db_agents_col, audit_dict_list, agent_dict_list, 
     # agent does not exist and has an audit (add agent to db)
     for agent in unique_agents:  # must see if i can condense later
         if agent in all_agents:
-            # agent exists and has an audit
-            # update using dictionary
-            pass
+            # agent exists and has a new audit
+            db_agents_col.update({"agent_name": agent}, {"avg_score": agent_dict_list["avg_score"], "total_inspection_count": agent_dict_list["total_inspection_count"], "time_series": agent_dict_list["time_series"]})
         else:
-            # agent does not exist and has an audit
-            # insert using full dictionary
-            pass
-
+            # agent does not exist and has a new audit
+            for agent_dict in agent_dict_list:
+                if agent_dict["agent_name"] == agent:
+                    db_agents_col.insert(agent_dict)
     for agent in all_agents:
         if agent not in unique_agents:
-            # agent exists and has no audit
-            # append 0 to timeseries
-            pass
+            # agent exists but has no new audit
+            time_series = db_agents_col.find({"agent_name": agent}, {"time_series": 1})
+            time_series[0].append(0.0000)
+            time_series[1].append(0)
+
 
 main()
