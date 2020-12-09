2582c96d290db85b5eefa450a2fb7330d70b4e32
calumGM <calum.maitland00@gmail.com>
2020-12-09 16:11:57 +1000

    inspections page with new data

--- NodeTutorial/routes/web/inspection.js
+++ NodeTutorial/routes/web/inspection.js
@@ -1,41 +1,29 @@
 var express = require("express");
 var passport = require("passport");
-var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated;
+//var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated;
 
 var Audit = require("../../models/audits");
 var Agent = require("../../models/agents");
 var router = express.Router();
-router.use(ensureAuthenticated);// ensures that all routes in this route are now authenticated
+//router.use(ensureAuthenticated);// ensures that all routes in this route are now authenticated
 var found_agents;
 var agent;
 // how do I talk to the routes in this page??
 router.get("/", function(req, res){ // implicit /post before each of these routes
     Agent.find({}).exec(function(err, agents){ // find all agents in database
         if(err){console.log(err);}
+        found_agents = agents;
         res.render("inspections/inspections", {agents:agents}); // passing on the posts that were found matching the userID
     });
  });
 
 router.get("/:agent_name", function(req,res){ // :postID represents a variable parameter as a route
-    Agent.find({}).exec(function(err, agents){ // find all agents in database
-        //doesnt work without console.log
-        // console.log('agents'); 
-        setTimeout(function(){ found_agents = agents; }, 3000);
-        found_agents = agents;
-        console.log('agents')
-        if(err){console.log(err);}
-    });
     Audit.find({agent_name:req.params.agent_name}).exec(function(err, audits){ // find all audits done by a particular agent
         if(err){console.log(err);}
         // do all calcs in here. Yes, because backend does all the heavy lifting
         var agent_data = {};
-        var audits_data = {};
         var i;
-        var daily_audit_count = {};
         var day_labels = [];
-        var daily_audit_data = [];
-        var sorted_day_labels = [];
-        var sorted_daily_audit_data = [];
         for (i=0; i<found_agents.length; i++){
             if (found_agents[i].agent_name === req.params.agent_name){
                 agent = found_agents[i]; // the agent in the URL
@@ -52,8 +40,7 @@ router.get("/:agent_name", function(req,res){ // :postID represents a variable p
         agent_data['agent_name'] = audits[0].agent_name;
 
         // generate date labels counting backwards since today
-        //console.log('time series', agent.time_series.length)
-        for (var i = agent.time_series.length; i > 0; i--){
+        for (var i = agent.time_series[0].length; i > 0; i--){
             var now = new Date();
             var previous_day = new Date();
             previous_day.setTime(now.getTime() - 1000*60*60*24*i); // milliseconds since 1970
@@ -67,15 +54,22 @@ router.get("/:agent_name", function(req,res){ // :postID represents a variable p
         for (i = 0; i<audits.length; i++){
             var long = audits[i]['location']['long']; 
             var lat = audits[i]['location']['lat'];
-            var addr = audits[i]['location']['address'];
+            var addr = audits[i]['location']['text'];
             audit_GPS_data[i] = {long, lat, addr};
         }
-        console.log('audit_GPS_data');
+        
         agent_data['GPS_data'] = audit_GPS_data;
-        
+        //console.log('audit_GPS_data', audit_GPS_data);
         agent_data['day_labels'] = day_labels;
-        agent_data['daily_audit_data'] = agent.time_series;
-        
+        agent_data['number_of_audits_per_day'] = agent.time_series[1];
+        agent_data['audit_score_per_day'] = agent.time_series[0];
+        if (audits.length === agent.time_series[1].length){
+            console.log('yay');
+        } else {
+            console.log('audits length:',audits.length);
+            console.log('time series length:', agent.time_series[1].length)
+        }
+
         res.render("inspections/view",{audits:audits, agent_data:agent_data, agents:found_agents});
     });
 });
